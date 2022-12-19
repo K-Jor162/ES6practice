a846fd0f2856b50b1d8dc8c35112e57ad097e152
K.J <117104691+K-Jor162@users.noreply.github.com>
2022-12-19 11:30:12 -0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+let cars = (masda, toyota, chevy) => {
+  return (
+    "Your first option of vehicle is ${masda}",
+    "Your second option is ${toyota}"
+  );
+};
+console.log(cars.masda);
+
+const firstName = "Keenan";
+const lastName = "Jordan";
+console.log("My first name is ${firstName}, and my last name is ${lastName}");
+
+const store = (name, city, age) => {
+  "My name is ${name} and I live in ${city}, and my age is ${age}";
+};
+
+console.log("Keenan Jordan", "Conroe Texas", "31 years old");
